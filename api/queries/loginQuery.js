a30ef3569e6453e92caaa65d4e278bead10a07ee
Dungntt2003 <nguyenthuydung55555@gmail.com>
2024-05-18 18:38:52 +0700

    CRUD api

--- api/queries/loginQuery.js
+++ api/queries/loginQuery.js
@@ -1,13 +1,16 @@
 const createNewUser = "INSERT INTO person (email, password) VALUES ($1,$2)";
 
-const checkUser = "SELECT password FROM person WHERE email = $1";
+const checkUser = "SELECT password, id FROM person WHERE email = $1";
 
-const getAllUsers ='SELECT email from person';
+const getAllUsers = "SELECT email from person";
 const checkDoctor = "SELECT password FROM doctor WHERE email = $1";
-
+const updateUser = "UPDATE person SET password = $1 where id = $2";
+const deleteUser = "DELETE FROM person WHERE id = $1";
 module.exports = {
   createNewUser,
   checkUser,
   getAllUsers,
-  checkDoctor
+  checkDoctor,
+  updateUser,
+  deleteUser,
 };
--- api/routes/login.js
+++ api/routes/login.js
@@ -5,11 +5,15 @@ const {
   checkUserLogin,
   getUser,
   checkDoctorLogin,
+  updateUserById,
+  deleteUserById,
 } = require("../controllers/loginController");
 
 router.post("/register", registerNewUser);
 router.post("/login", checkUserLogin);
-router.get("/users", getUser)
+router.get("/users", getUser);
+router.put("/users/:id", updateUserById);
+router.delete("/users/:id", deleteUserById);
 router.post("/login/doctor", checkDoctorLogin);
 
 module.exports = router;
