72798cb622e78f27ec4781c632b5bbb39c9c822d
anhthunguyenngoc <anhthunguyenngoc80@gmail.com>
2024-05-22 14:32:58 +0700

    update

--- api/controllers/loginController.js
+++ api/controllers/loginController.js
@@ -43,28 +43,25 @@ const checkUserLogin = (req, res, next) => {
     if (result.rows.length > 0) {
       const hash = result.rows[0].password;
       const id = result.rows[0].id;
-      bcrypt.compare(password, hash, function (err, result) {
+      bcrypt.compare(password, hash, function (err, result1) {
         if (err) {
           res.status(500).json({
             message: err.message,
           });
           return;
         }
-        if (result) {
-          res.status(200).json({
-            message: "Login successfully",
-            id: id,
-          });
+        if (result1) {
+          res.status(200).json(result.rows);
           return;
         }
-        res.status(401).json({
+        res.status(401).json([{
           message: "Wrong password",
-        });
+        }]);
       });
     } else {
-      res.status(409).json({
+      res.status(409).json([{
         message: "Invalid email",
-      });
+      }]);
     }
   });
 };
