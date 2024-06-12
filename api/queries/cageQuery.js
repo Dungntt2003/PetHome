b0d373325393525113353213548a55d38ac3c646
Dungntt2003 <nguyenthuydung55555@gmail.com>
2024-06-12 21:01:59 +0700

    Update api

--- api/queries/cageQuery.js
+++ api/queries/cageQuery.js
@@ -9,13 +9,13 @@ const getFreeHotel = `SELECT DISTINCT cage.cage_id FROM cage LEFT JOIN bookHotel
                         WHERE endDate <= CURRENT_DATE OR endDate is null`;
 
 const getDeadHotel =
-  'SELECT * FROM cage join bookHotel on cage.cage_id = bookHotel.cage_id where endDate = current_date - interval "1 day"';
+  'SELECT * FROM cage join bookHotel on cage.cage_id = bookHotel.cage_id where endDate = current_date - interval "1 day" and endDate is not null';
 
 const upgradeCage = "UPDATE cage set hotel_id = $1 where cage_id = $2";
 
 const deleteCage = "DELETE FROM cage WHERE cage_id = $1";
 const getLiveCage =
-  "SELECT DISTINCT cage.cage_id FROM bookHotel join bookschedule using (id) WHERE endDate > CURRENT_DATE";
+  "SELECT DISTINCT bookHotel.cage_id FROM bookHotel join bookschedule using (id) WHERE endDate > CURRENT_DATE";
 
 module.exports = {
   insertCage,
